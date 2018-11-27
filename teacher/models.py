9e4b7ca54e35968c852938ac16c69ff909a329b5
cuongdaovan <cuongdaovan262@gmail.com>
2018-11-27 11:30:48 +0700

    modify teacher model

--- teacher/models.py
+++ teacher/models.py
@@ -11,15 +11,26 @@ class Teacher(models.Model):
     country = models.CharField(max_length=50)
     teach_start_date = models.DateField()
     education = models.TextField(max_length=1000)
-    level = ('1', '2', '3')
-    sex = ('female', 'male')
+    l = (
+        (1, 'level 1'),
+        (2, 'level 2'),
+        (3, 'level 3'),
+    )
+    level = models.IntegerField(choices=l, default=1, blank=True)
+    s = (
+        ('female', 'female'),
+        ('male', 'male'),
+    )
+    sex = models.CharField(max_length=10, choices=s, default='', blank=True)
     date_of_birth = models.DateField()
     faculty_code = models.ForeignKey(
         school_model.Faculty,
         on_delete=models.CASCADE,
         related_name='faculties'
     )
+    email = models.EmailField(default='', blank=True)
     password = models.CharField(max_length=50)
+
 
 # class Evaluation(models.Model):
 #     evaluation_code = models.AutoField(primary_key=True)
