147d91d46437c27a2365571aeec81341b778ba1f
cuongdaovan <cuongdaovan262@gmail.com>
2018-11-29 13:34:00 +0700

    modify setting for cors

--- ESM/settings.py
+++ ESM/settings.py
@@ -58,6 +58,15 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+)
+CORS_ORIGIN_REGEX_WHITELIST = (
+    'localhost:3000',
+)
+
 ROOT_URLCONF = 'ESM.urls'
 
 TEMPLATES = [
