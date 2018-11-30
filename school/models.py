d0cf0e1826269750aed9ade35d56cde55a921e60
cuongdaovan <cuongdaovan262@gmail.com>
2018-11-30 14:44:38 +0700

    have conflict when delete foreign key, re-create database

--- school/models.py
+++ school/models.py
@@ -14,18 +14,19 @@ class Subject(models.Model):
     subject_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, blank=False)
     description = models.TextField(max_length=1000, blank=True)
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='subjects')
 
     def __str__(self):
         return self.name
 
 
 class Module(models.Model):
+    module_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     subject = models.ForeignKey(Subject, related_name='modules', on_delete=models.CASCADE)
     description = models.TextField(max_length=1000)
     credit = models.IntegerField()
     time = models.IntegerField(default=0)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
