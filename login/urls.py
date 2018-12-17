2e5165250512b82b49ee44663b06c84efe45c622
cuongdaovan <cuongdaovan262@gmail.com>
2018-12-17 14:50:53 +0700

    wrote api for login

--- login/urls.py
+++ login/urls.py
@@ -1,9 +1,14 @@
-from rest_framework_simplejwt import views as jwt_view
-
 from django.urls import path
 
 from . import views
 
+from rest_framework_simplejwt import views as jwt_view
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('api/v1/login', views.LoginAPI, basename='User')
+
 urlpatterns = [
 
 ]
+urlpatterns += router.urls
--- login/views.py
+++ login/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth import views
 from django.contrib.auth import (
     login as auth_login,
+    logout as auth_logout
 )
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 
 from rest_framework_simplejwt import serializers as jwt_s
 from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from rest_framework import viewsets
+
+from . import serializers
 
 
 class LoginView(views.LoginView):
@@ -20,3 +25,21 @@ class LoginView(views.LoginView):
         response.set_cookie('refresh', token_r)
         response.set_cookie('access', token_a)
         return response
+
+
+class LogoutView(views.LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        auth_logout(request)
+        next_page = self.get_next_page()
+        if next_page:
+            # Redirect to this page until the session has been cleared.
+            response = HttpResponseRedirect('http://127.0.0.1:8080/api-auth/login/?next=%s' % next_page)
+            response.set_cookie('access', ' ')
+            response.set_cookie('refresh', ' ')
+            return response
+        return super().dispatch(request, *args, **kwargs)
+
+
+class LoginAPI(viewsets.ModelViewSet):
+    serializer_class = serializers.LoginSerializer
+    queryset = User.objects.all()
